1aad7b7ba07b5d224b8176aa8d37b6e4e0c0ad9d
yumeow0122 <68141436+yumeow0122@users.noreply.github.com>
2022-05-23 20:05:41 +0800

    finish python

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,1 +1,38 @@
-print("Fuck this shit.\n")
+import csv
+import pandas as pd
+
+def get_level(num):
+    if(num >= 90):
+        return 'A+'
+    elif(num >= 85):
+        return 'A'
+    elif(num >= 80):
+        return 'A-'
+    elif(num >= 77):
+        return 'B+'
+    elif(num >= 73):
+        return 'B'
+    elif(num >= 70):
+        return 'B-'
+    elif(num >= 67):
+        return 'C+'
+    elif(num >= 63):
+        return 'C'
+    elif(num >= 60):
+        return 'C-'
+    elif(num >= 50):
+        return 'D'
+    else:
+        return 'E'
+
+df = pd.read_csv('HW2data.csv')
+
+total = []
+level = []
+for i in range(0, len(df)):
+    total.append(0.1*(df['HW1'][i]+df['HW2'][i]+df['HW3'][i]) + 0.3*df['Midterm'][i] + 0.4*df['Final'][i])
+    level.append(get_level(total[i]))
+
+df['Total'] = total
+df['Level'] = level
+print(df)
