1d690558d7dce50a5d55dacbf80fd687d404be12
Avvco <leei910110@gmail.com>
2022-05-23 22:25:17 +0800

    update input file path

--- JAVA/app/src/main/java/java/FileIn.java
+++ JAVA/app/src/main/java/java/FileIn.java
@@ -6,9 +6,9 @@ public class FileIn {
   public static String inputBaseFilePath = "./app";
 
   public static void checkinputFilePath() {
-    if(System.getProperty("user.dir").substring(System.getProperty("user.dir").lastIndexOf("/") + 1).equals("lib")){
+    if(System.getProperty("user.dir").substring(System.getProperty("user.dir").lastIndexOf("/") + 1).equals("app")){
       inputBaseFilePath = "./";
-    }else if(System.getProperty("user.dir").substring(System.getProperty("user.dir").lastIndexOf("\\") + 1).equals("lib")){
+    }else if(System.getProperty("user.dir").substring(System.getProperty("user.dir").lastIndexOf("\\") + 1).equals("app")){
       inputBaseFilePath = "./";
     }
   }
