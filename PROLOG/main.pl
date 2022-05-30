2671ddbaeb29b24e916c9eb6f15f0c27d8286281
Sowa 鴞 <wanderwalker0110@protonmail.com>
2022-05-30 18:49:01 +0800

    prolog score caculator ver1

--- PROLOG/main.pl
+++ PROLOG/main.pl
@@ -1,14 +1,23 @@
-q:- L = [33, 18, 2, 77, 66, 18, 9, 25], last(P,_), (quicksort(L,P,_), write(P), nl), halt.
-partition([], _, [], []).
-partition([X|Xs], Pivot, Smalls, Bigs) :-
-    (   X @< Pivot ->
-        Smalls = [X|Rest],
-        partition(Xs, Pivot, Rest, Bigs)
-    ;   Bigs = [X|Rest],
-        partition(Xs, Pivot, Smalls, Rest)
-    ).
-quicksort([])     --> [].
-quicksort([X|Xs]) -->
-    { partition(Xs, X, Smaller, Bigger) },
-    quicksort(Smaller), [X], quicksort(Bigger).
-:- initialization(q).
+start:-
+  read_score(X) ,
+  grade_level(X,R),
+  write('Your grade is '),
+  write(R),
+  nl.
+
+read_score(X) :-
+  write('please enter your score: '),
+  read(X),
+  number(X).
+
+grade_level( X , 'A+' ) :- X >= 90, X<  101.
+grade_level( X , 'B ' ) :- X >= 85, X <  90.
+grade_level( X , 'A-' ) :- X >= 80, X <  84.
+grade_level( X , 'B+' ) :- X >= 77, X <  79.
+grade_level( X , 'B ' ) :- X >= 73, X <  76.
+grade_level( X , 'B-' ) :- X >= 70, X <  72.
+grade_level( X , 'C+' ) :- X >= 67, X <  69.
+grade_level( X , 'C ' ) :- X >= 63, X <  66.
+grade_level( X , 'C-' ) :- X >= 60, X <  62.
+grade_level( X , 'D ' ) :- X >= 50, X <  59.
+grade_level( X , 'E ' ) :- X >= 0 , X <  49.
