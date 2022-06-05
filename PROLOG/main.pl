d58c1d6ae637604bc176aaf1c9258124ffbda930
Sowa 鴞 <wanderwalker0110@protonmail.com>
2022-06-05 18:49:20 +0800

    bug fixed
    type run. to run program

--- PROLOG/main.pl
+++ PROLOG/main.pl
@@ -1,40 +1,40 @@
-% 把他們的成雞放入knowledge base
-% start -> run
-% Score = HW1 * 0.1 + HW2 * 0.1 + HW3 * 0.1 + Midterm * 0.3 + Final * 0.4
-% bug: 第3筆跟第9筆無法輸出
+% HW2 using prolog to implement
+% type "run." to start
+
 truncate(X,N,Result):- X >= 0, Result is floor(10^N*X)/10^N, !.   %digit ctrl
 
-:- use_module(library(csv)).
-import:-
-    csv_read_file("HW2data.csv", Rows, [functor(score), arity(8)]),
-    maplist(assert, Rows).
 
-readscore(10):- score(10,B,C,D,E,F,G,H),
-  truncate(D*0.1+E*0.1+F*0.1+G*0.3+H*0.4, 2, Gra), truncate(D*0.1+E*0.1+F*0.1+G*0.3+H*0.4, 0, Gra1),
-  grade_level(integer(Gra1), Lev),
-  write(10), write('\t'), write(B), write('\t'), write(C), write('\t'), write(D), write('\t'),
-  write(E), write('\t'), write(F), write('\t'), write(G), write('\t'), write(H), write('\t'), 
+:- use_module(library(csv)).
+run:-
+    csv_read_file("HW2data.csv", Rows, [functor(score), arity(8)]),
+    maplist(assert, Rows), X is 1,
+    calculate_score(X), halt.
+
+
+calculate_score(10):- score(10,B,C,D,E,F,G,H),
+  truncate(D*0.1+E*0.1+F*0.1+G*0.3+H*0.4, 2, Gra),
+  grade_level(Gra, Lev),
+  write(10), write('\t'), write(B), write('\t'), write(C), write('\t'),
   write(Gra), write('\t'), write(Lev), write('\t'),
   nl.
 
-readscore(X):-
+calculate_score(X):-
   Y is X, score(Y,B,C,D,E,F,G,H), 
-  truncate(D*0.1+E*0.1+F*0.1+G*0.3+H*0.4, 2, Gra), truncate(D*0.1+E*0.1+F*0.1+G*0.3+H*0.4, 0, Gra1),
-  grade_level(integer(Gra1), Lev),
-  write(Y), write('\t'), write(B), write('\t'), write(C), write('\t'), write(D), write('\t'),
-  write(E), write('\t'), write(F), write('\t'), write(G), write('\t'), write(H), write('\t'),
+  truncate(D*0.1+E*0.1+F*0.1+G*0.3+H*0.4, 2, Gra),
+  grade_level(Gra, Lev),
+  write(Y), write('\t'), write(B), write('\t'), write(C), write('\t'),
   write(Gra), write('\t'), write(Lev), write('\t'),
   Z is Y+1, nl,
-  readscore(Z).
-% write(Gra), write('\t'), 
-grade_level( X , 'A+' ) :- X >= 90, X <  101.
-grade_level( X , 'B ' ) :- X >= 85, X <  90.
-grade_level( X , 'A-' ) :- X >= 80, X <  84.
-grade_level( X , 'B+' ) :- X >= 77, X <  79.
-grade_level( X , 'B ' ) :- X >= 73, X <  76.
-grade_level( X , 'B-' ) :- X >= 70, X <  72.
-grade_level( X , 'C+' ) :- X >= 67, X <  69.
-grade_level( X , 'C ' ) :- X >= 63, X <  66.
-grade_level( X , 'C-' ) :- X >= 60, X <  62.
-grade_level( X , 'D ' ) :- X >= 50, X <  59.
-grade_level( X , 'E ' ) :- X >= 0 , X <  49.
+  calculate_score(Z).
+
+grade_level( X , 'A+' ) :- X >= 90.0, X <  101.0.
+grade_level( X , 'B ' ) :- X >= 85.0, X <  90.0.
+grade_level( X , 'A-' ) :- X >= 80.0, X <  85.0.
+grade_level( X , 'B+' ) :- X >= 77.0, X <  80.0.
+grade_level( X , 'B ' ) :- X >= 73.0, X <  77.0.
+grade_level( X , 'B-' ) :- X >= 70.0, X <  73.0.
+grade_level( X , 'C+' ) :- X >= 67.0, X <  70.0.
+grade_level( X , 'C ' ) :- X >= 63.0, X <  67.0.
+grade_level( X , 'C-' ) :- X >= 60.0, X <  63.0.
+grade_level( X , 'D ' ) :- X >= 50.0, X <  60.0.
+grade_level( X , 'E ' ) :- X >=  0.0, X <  50.0.
